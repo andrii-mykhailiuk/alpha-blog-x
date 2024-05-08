cb54ba6d8ac4134559111f85cf01801506b3b209
Andrii Mykhailiuk <an5532583@gmail.com>
2024-05-09 02:01:26 +0300

    Added all REST methods, refactored views and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        # debugger
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -13,7 +13,6 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
@@ -27,14 +26,28 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
-            redirect_to @article, status: 202
+            redirect_to @article
         else
+            flash[:notice] = "Article wasn't updated."
             render 'edit', status: 400
         end
     end
 
+    def destroy
+        @article.destroy
+        redirect_to articles_path, status: :see_other
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 
 end
